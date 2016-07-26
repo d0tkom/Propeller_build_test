c29d5c2583702c5f4cae6955e44458b49a29fcc1
Patrik Singer <dotkom@Patriks-MBP.home>
2016-07-26 02:21:29 +0100

    carousel

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
+var position = 0;
 $(document).ready(function(){
 	// Set the interval to be 5 seconds
 	var t = setInterval(function(){
-		$("#carousel ul").animate({marginLeft:-1240},1000,function(){
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
+		carousel(1);
+		position++;
+		if (position==6) {
+			position = 0;
+			document.getElementById(5).src = "./image/rectangle.png";
+		}
+		else {
+			document.getElementById(position-1).src = "./image/rectangle.png";
+		}
+		document.getElementById(position).src = "./image/on.png";
+		console.log(position);
 	},3000);
 });
+
+function carousel(x) {
+	$("#carousel ul").animate({marginLeft:-x*1240},1000,function(){
+		$(this).find("li:last").after($(this).find("li:first"));
+		$(this).css({marginLeft:0});
+	})
+}
