6fb5462b7d52ebd464a9a63c2de6ce9d88758b56
Patrik Singer <dotkom@Patriks-MBP.home>
2016-07-26 18:48:25 +0100

    carousel
    fix carousel

--- script/script.js
+++ script/script.js
@@ -1,40 +1,23 @@
-var position = 0;
-var autoplay = true;
 $(document).ready(function(){
-	// Set the interval to be 5 seconds
-	if (autoplay) {
-	setInterval(function(){
-		carousel(1);
-		position++;
-		if (position==6) {
-			position = 0;
-			document.getElementById(5).src = "./image/rectangle.png";
-		}
-		else {
-			document.getElementById(position-1).src = "./image/rectangle.png";
-		}
-		document.getElementById(position).src = "./image/on.png";
-		console.log(position);
-	},3000);
-	}
+$(".slider").slick({
+
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    customPaging : function(slider, i) {
+        var thumb = $(slider.$slides[i]).data('thumb');
+        return '<a><img src="'+thumb+'"></a>';
+    },
+
+    responsive: [{ 
+        breakpoint: 500,
+        settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        } 
+    }]
 });
-
-function carousel(x) {
-	$("#carousel ul").animate({marginLeft:-x*1240},1000,function(){
-		$(this).find("li:last").after($(this).find("li:first"));
-		$(this).css({marginLeft:0});
-	})
-}
-
-function move_carousel(x) {
-	var move = 0;
-
-	$("#carousel ul").animate({marginLeft:-(x-position)*1240},1000,function(){
-		$(this).find("li:last").after($(this).find("li:first"));
-		$(this).css({marginLeft:0});
-		x = position;
-	})
-	
-}
-
-
+});
